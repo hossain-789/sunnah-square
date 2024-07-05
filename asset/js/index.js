9f084f55fc57d640d733d33d297077854441b6be
Hossain Ahmed <hossainahmed.7709@gmail.com>
2024-07-06 00:07:48 +0600

    completed ecommarce web

---
+++ asset/js/index.js
@@ -0,0 +1,10 @@
+//lost-password
+document.getElementById('getResetPasswod').addEventListener('click', () => {
+    event.preventDefault()
+    let emailId = document.getElementById('getEmailAddress').value;
+    if (emailId.length <= 5) {
+        document.getElementById('subscribError').innerHTML = '<i class="bi bi-bell"></i> Enter a username or email address.';
+    } else {
+        document.getElementById('subscribError').innerHTML = '<i class="bi bi-bell"></i> Invalid username or email.';
+    }
+})
